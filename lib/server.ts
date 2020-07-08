7ce1e56f7337f37399a518b00e4c808ecb8e618c
Ian <ianguimaraes31@gmail.com>
2020-07-07 23:01:54 -0300

    Removed unused file

--- lib/server.ts
+++ lib/server.ts
@@ -4,13 +4,8 @@ import http from 'http';
 
 const PORT = 8080
 
-// const server = app.listen(PORT);
-
 const server = http.createServer(app);
 
 server.listen(PORT, () => console.log('Listening request'));
 
 export const io = socketIo(server);
-
-
-
