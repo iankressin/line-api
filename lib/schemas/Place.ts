7ffc4101e79d393160731ccf939a2a756be9c486
Ian <ianguimaraes31@gmail.com>
2020-07-09 00:31:00 -0300

    Added city, address, state, phoneNumber and imagePath to user Schema

--- lib/schemas/Place.ts
+++ lib/schemas/Place.ts
@@ -3,17 +3,27 @@ import { Schema, model, Document } from 'mongoose';
 interface IPlace extends Document {
   email: string,
   name: string,
+  imagePath: String,
   state: boolean,
   cnpj: string,
+  phoneNumber: string,
+  address: string,
+  city: string,
+  stateOrProvince: string,
   queue: Schema.Types.ObjectId[],
 }
 
 const PlaceSchema = new Schema({
   email: String,
   name: String,
+  imagePath: String,
   open: { type: Boolean, default: false},
   cnpj: String, 
-  
+  phoneNumber: String,
+  address: String,
+  city: String,
+  stateOrProvince: String,
+   
   queue: {
     type: [Schema.Types.ObjectId],
     ref: 'User',
